a258553ab027db5052c5bb1129156135ac9b950c
Dave Thaler <dthaler@microsoft.com>
2023-10-04 13:14:19 -0700

    Add basic memory usage test
    Signed-off-by: Dave Thaler <dthaler@microsoft.com>

---
+++ Tests/BasicTests.cs
@@ -0,0 +1,54 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Gedcom7;
+using System.Diagnostics;
+using System;
+
+namespace Tests
+{
+    [TestClass]
+    public class BasicTests
+    {
+        private TestContext testContextInstance;
+
+        public TestContext TestContext
+        {
+            get { return testContextInstance; }
+            set { testContextInstance = value; }
+        }
+
+        void TestMemoryUsage(string fileName, long maxSize)
+        {
+            long beforeMemory = GC.GetTotalMemory(true);
+            TestContext.WriteLine($"Baseline: {beforeMemory} bytes");
+
+            var note = new GedcomFile();
+
+            // Fetch new process information.
+            long emptyMemory = GC.GetTotalMemory(true);
+            long used = emptyMemory - beforeMemory;
+            TestContext.WriteLine($"Empty GedcomFile: {used} bytes");
+            Assert.IsTrue(used <= 424);
+
+            bool ok = note.LoadFromPath(fileName);
+            Assert.IsTrue(ok);
+
+            // Fetch new process information.
+            long afterMemory = GC.GetTotalMemory(true);
+            used = afterMemory - emptyMemory;
+            TestContext.WriteLine($"Full GedcomFile: +{used} bytes");
+            Assert.IsTrue(used <= maxSize);
+        }
+
+        [TestMethod]
+        public void Minimal70MemoryUsage()
+        {
+            TestMemoryUsage("../../../../external/GEDCOM.io/testfiles/gedcom70/minimal70.ged", 1376);
+        }
+
+        [TestMethod]
+        public void Maximal70MemoryUsage()
+        {
+            TestMemoryUsage("../../../../external/GEDCOM.io/testfiles/gedcom70/maximal70.ged", 268040);
+        }
+    }
+}
