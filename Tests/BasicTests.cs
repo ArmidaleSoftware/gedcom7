1aa6384d9daeeb34e765fe14fcc046a39aef8ace
Dave Thaler <dthaler@microsoft.com>
2023-10-24 11:50:34 +0300

    Update memory size requirement
    Signed-off-by: Dave Thaler <dthaler@microsoft.com>

--- Tests/BasicTests.cs
+++ Tests/BasicTests.cs
@@ -53,7 +53,7 @@ namespace Tests
         [TestMethod]
         public void Maximal70MemoryUsage()
         {
-            TestMemoryUsage("../../../../external/GEDCOM.io/testfiles/gedcom70/maximal70.ged", 218464);
+            TestMemoryUsage("../../../../external/GEDCOM.io/testfiles/gedcom70/maximal70.ged", 218984);
         }
     }
 }
