2933c29001386076f70a144f989115ef266c9fa3
Dave Thaler <dthaler1968@gmail.com>
2023-11-25 18:07:00 -0800

    Validate enumeration values (#116)
    * Validate enumeration values
    
    Signed-off-by: Dave Thaler <dthaler@armidalesoftware.com>
    
    * Validate list of Enum
    
    Signed-off-by: Dave Thaler <dthaler@armidalesoftware.com>
    
    * Change GEDCOM-registries to temporarily use dthaler fork
    
    Signed-off-by: Dave Thaler <dthaler@armidalesoftware.com>
    
    ---------
    
    Signed-off-by: Dave Thaler <dthaler@armidalesoftware.com>
    Co-authored-by: Dave Thaler <dthaler@armidalesoftware.com>

---
+++ Gedcom7/EnumerationSet.cs
@@ -0,0 +1,68 @@
+﻿// Copyright (c) Armidale Software
+// SPDX-License-Identifier: MIT
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Yaml.Serialization;
+
+namespace Gedcom7
+{
+    public class EnumerationSet
+    {
+        public string Uri { get; private set; }
+        public override string ToString() => this.Uri;
+        public List<string> ValueUris { get; private set; }
+        public List<string> ValueTags { get; private set; }
+        EnumerationSet(Dictionary<object, object> dictionary)
+        {
+            this.Uri = dictionary["uri"] as string;
+            this.ValueUris = new List<string>();
+            GedcomStructureSchema.AddStrings(this.ValueUris, dictionary["enumeration values"] as Object[]);
+
+            this.ValueTags = new List<string>();
+            foreach (var uri in this.ValueUris)
+            {
+                // First try an enumeration value.
+                EnumerationValue value = EnumerationValue.GetEnumerationValue(uri);
+                if (value != null)
+                {
+                    this.ValueTags.Add(value.StandardTag);
+                    continue;
+                }
+
+                // Now try a structure URI.
+                GedcomStructureSchema schema = GedcomStructureSchema.GetSchema(uri);
+                this.ValueTags.Add(schema.StandardTag);
+            }
+        }
+
+        static Dictionary<string, EnumerationSet> s_EnumerationSets = new Dictionary<string, EnumerationSet>();
+
+        public static void LoadAll()
+        {
+            if (s_EnumerationSets.Count > 0)
+            {
+                return;
+            }
+            EnumerationValue.LoadAll();
+            string currentDirectory = Directory.GetCurrentDirectory();
+            var path = Path.Combine(currentDirectory, "../../../../external/GEDCOM-registries/enumeration-set/standard");
+            string[] files = Directory.GetFiles(path);
+            foreach (string filename in files)
+            {
+                var serializer = new YamlSerializer();
+                object[] myObject = serializer.DeserializeFromFile(filename);
+                var dictionary = myObject[0] as Dictionary<object, object>;
+                var schema = new EnumerationSet(dictionary);
+                s_EnumerationSets.Add(schema.Uri, schema);
+            }
+        }
+
+        public static EnumerationSet GetEnumerationSet(string uri) => s_EnumerationSets[uri];
+
+        public bool IsValidValue(string value)
+        {
+            return this.ValueTags.Contains(value);
+        }
+    }
+}
